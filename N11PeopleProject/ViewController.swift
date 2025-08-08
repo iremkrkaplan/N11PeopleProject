936c37cdb9e24769d15118fec95efed390b0cc05
iremkarakaplan <iremkrkaplan@gmail.com>
2025-08-08 09:37:13 +0300

    Initial Commit

---
+++ N11PeopleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N11PeopleProject
+//
+//  Created by irem.karakaplan on 8.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
