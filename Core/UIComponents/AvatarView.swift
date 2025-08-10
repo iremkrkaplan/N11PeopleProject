80973f8ade18ea1a54dbbb2202a2ee34f5ca751c
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-08-10 22:41:31 +0300

    Revised DashboardViewController

---
+++ Core/UIComponents/AvatarView.swift
@@ -0,0 +1,20 @@
+//
+//  AvatarView.swift
+//  N11PeopleProject
+//
+//  Created by irem.karakaplan on 10.08.2025.
+//
+
+import UIKit
+
+class AvatarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
