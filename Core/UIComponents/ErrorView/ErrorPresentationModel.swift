7791d368c18a6dd1a84556e5871c15e98709e0d8
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-08-21 13:50:15 +0300

    Added ErrorView to UIComponents, Created ErrorPresentationModel struct

---
+++ Core/UIComponents/ErrorView/ErrorPresentationModel.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorPresentationModel.swift
+//  N11PeopleProject
+//
+//  Created by irem.karakaplan on 21.08.2025.
+//
+
+import UIKit
+
+struct ErrorPresentationModel {
+    let titleViewText: String
+    let subtitleViewText: String
+    let imageView: UIImage
+    let retryAction: () -> Void
+}
---
+++ Core/UIComponents/ErrorView/ErrorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorViewController.swift
+//  N11PeopleProject
+//
+//  Created by irem.karakaplan on 21.08.2025.
+//
+
+import UIKit
+
+class ErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
