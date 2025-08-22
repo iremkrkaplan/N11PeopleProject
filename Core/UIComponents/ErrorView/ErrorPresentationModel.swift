555cb9b853c9e6844cb560b888b673c4f05bb8c6
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-08-22 15:05:46 +0300

    Updated the ErrorPresentationModel

--- Core/UIComponents/ErrorView/ErrorPresentationModel.swift
+++ Core/UIComponents/ErrorView/ErrorPresentationModel.swift
@@ -10,6 +10,28 @@ import UIKit
 struct ErrorPresentationModel {
     let titleViewText: String
     let subtitleViewText: String
-    let imageView: UIImage
-    let retryAction: () -> Void
+    let imageView: UIImage?
+    let retryButtonModel: RetryActionPresentationModel
+}
+
+struct RetryActionPresentationModel {
+    let buttonTitle: String
+    let action: () -> Void
+}
+
+//TODO: Will be moved to presenter
+extension ErrorPresentationModel{
+    
+    static func createViewData() -> ErrorPresentationModel {
+        return .init(titleViewText: "Oops!",
+                     subtitleViewText: "There is no connection",
+                     imageView:  UIImage(named: "NetworkErrorImage") ?? UIImage(systemName: "wifi.exclamationmark"),
+                     retryButtonModel: createPlaceholderRetryButtonModel())
+    }
+    
+    static func createPlaceholderRetryButtonModel() -> RetryActionPresentationModel {
+        return .init(buttonTitle: "Retry", action: {
+            print("Retry button tapped")
+        })
+    }
 }
