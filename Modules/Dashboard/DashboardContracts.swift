4636f1d38a923f86df6adb1db89846a572ca7559
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-08-25 16:51:43 +0300

    Added Protocols

--- Modules/Dashboard/DashboardContracts.swift
+++ Modules/Dashboard/DashboardContracts.swift
@@ -5,6 +5,28 @@
 //  Created by irem.karakaplan on 15.08.2025.
 //
 
+// MARK: - View Protocol
+protocol DashboardViewProtocol: AnyObject {
+    func displayLoading()
+    func bind(viewData: DashboardViewData)
+    func displayError(_ errorModel: ErrorPresentationModel)
+}
+
+// MARK: - Presenter Protocol
+protocol DashboardPresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func handleRefresh()
+    func settingsButtonTapped()
+}
+
+// MARK: - Interactor Protocol
 protocol DashboardInteractorProtocol: AnyObject {
-    func fetchAuthenticatedUser() async throws -> User
+    var presenter: DashboardInteractorOutputProtocol? { get set }
+    func fetchAuthenticatedUser()
+}
+
+// MARK: - Interactor Output Protocol
+protocol DashboardInteractorOutputProtocol: AnyObject {
+    func didFetchUser(user: User)
+    func didFailToFetchUser(error: Error)
 }
