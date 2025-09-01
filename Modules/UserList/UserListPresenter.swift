337973775eb99e6e1f3fdade93f41120063ea44d
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-09-01 14:05:49 +0300

    Refactor: Added Favorite logic to presenter(#27)

--- Modules/UserList/UserListPresenter.swift
+++ Modules/UserList/UserListPresenter.swift
@@ -13,19 +13,24 @@ final class UserListPresenter: UserListViewOutput, UserListInteractorOutput {
     private let interactor: UserListInteractorInput
     private let router: UserListRouterInput
     private var users: [User] = []
-
+    private let favoriteService: FavoriteStorageServiceProtocol
+    
     init(view: UserListViewInput,
          interactor: UserListInteractorInput,
-         router: UserListRouterInput) {
+         router: UserListRouterInput,
+         favoriteService: FavoriteStorageServiceProtocol = FavoriteStorageService.shared) {
+        
         self.view = view
         self.interactor = interactor
         self.router = router
+        self.favoriteService = favoriteService
     }
-        
+    
     func viewDidLoad() {
         let initialData = UserListViewData(
             title: "Kullanıcı Ara",
             searchPlaceholder: "GitHub kullanıcısı ara..."
+            //            TODO: Animation gif
         )
         view?.displayInitialState(with: initialData)
     }
@@ -37,20 +42,18 @@ final class UserListPresenter: UserListViewOutput, UserListInteractorOutput {
         interactor.searchUsers(with: query)
     }
     
+    func favoriteButtonTapped(for model: UserListCellModel) {
+        interactor.toggleFavorite(for: model.username)
+    }
+    
     func didSelectUser(with model: UserListCellModel) {
         router.navigateToUserDetail(username: model.username)
     }
     
+    
     func didFetchUsersSuccessfully(users: [User]) {
         self.users = users
-        
-        let cellModels = users.map { user -> UserListCellModel in
-            return UserListCellModel(
-                id: user.id,
-                username: user.login,
-                avatarURL: URL(string: user.avatarUrl)
-            )
-        }
+        let cellModels = mapUsersToCellModels(from: users)
         
         view?.displayLoading(false)
         
@@ -64,7 +67,23 @@ final class UserListPresenter: UserListViewOutput, UserListInteractorOutput {
     func didFailToFetchUsers(error: Error) {
         view?.displayLoading(false)
         view?.displayError(title: "Hata Oluştu", message: "Arama sırasında bir sorun oluştu. Lütfen tekrar deneyin.")
-
+        
         print("Error from Interactor: \(error.localizedDescription)")
     }
+    
+    func favoriteStatusDidChange() {
+        let updatedCellModels = mapUsersToCellModels(from: self.users)
+        view?.bind(results: updatedCellModels)
+    }
+    private func mapUsersToCellModels(from users: [User]) -> [UserListCellModel] {
+        return users.map { user in
+            UserListCellModel(
+                id: user.id,
+                username: user.login,
+                avatarURL: URL(string: user.avatarUrl),
+                isFavorite: favoriteService.isFavorite(username: user.login)
+            )
+        }
+    }
+    
 }
