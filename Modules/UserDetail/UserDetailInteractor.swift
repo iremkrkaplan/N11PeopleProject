31b16a97f32eb8ca2d26cb900436f5492779d685
İrem Karakaplan <iremkrkaplan@gmail.com>
2025-09-08 09:51:08 +0300

    Implement UserDetail Interactor #38

--- Modules/UserDetail/UserDetailInteractor.swift
+++ Modules/UserDetail/UserDetailInteractor.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class UserDetailAPIInteractor: UserDetailInteractorInput {
+    
+    weak var output: UserDetailInteractorOutput?
+    private let apiClient: APIClient
+    private let favoriteService: FavoriteStorageServiceProtocol
+    private let username: String
+    
+    init(username: String,
+         apiClient: APIClient,
+         favoriteService: FavoriteStorageServiceProtocol = FavoriteStorageService.shared) {
+        self.username = username
+        self.apiClient = apiClient
+        self.favoriteService = favoriteService
+    }
+    
+    func fetchUserDetail() {
+        Task {
+            do {
+                let params = GetUserDetailParams(username: self.username)
+                let detail = try await apiClient.getUserDetail(params)
+                await MainActor.run {
+                    output?.didFetchUserDetailSuccessfully(detail: detail)
+                }
+            } catch {
+                await MainActor.run {
+                    output?.didFailToFetchUserDetail(error: error)
+                }
+            }
+        }
+    }
+    
+    func toggleFavoriteStatus() {
+        favoriteService.toggleFavorite(username: self.username)
+    }
+}
